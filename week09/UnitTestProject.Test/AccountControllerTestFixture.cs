403664514b477e76df302d87c48c7aa1d5ec4d1a
Zénó Nagy <nagy.zeno999@gmail.com>
2020-11-22 15:17:46 +0100

    1-5

---
+++ week09/UnitTestProject.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UnitTestExample.Controllers;
+
+namespace UnitTestProject.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            var accountController = new AccountController();
+            var actualResult = accountController.ValidateEmail(email);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
